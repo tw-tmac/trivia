5e6b4336dc71f5fb22b6b5d119db253ec9390244
Thien-An Mac <tmac@thoughtworks.com>
2017-06-08 14:55:20 -0400

    Add TODO comments for facilator to work with :)

--- java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
@@ -10,7 +10,7 @@ public class Game {
 
     private ArrayList<Player> players = new ArrayList<>();
 
-    //make it a question class
+    //TODO: make it a question class
     private LinkedList<String> popQuestions = new LinkedList<>();
     private LinkedList<String> scienceQuestions = new LinkedList<>();
     private LinkedList<String> sportsQuestions = new LinkedList<>();
@@ -20,7 +20,6 @@ public class Game {
     private Player currentPlayer;
     public  Game(){
 
-    	//Create questions outside of the class
     	for (int i = 0; i < 50; i++) {
 			popQuestions.addLast("Pop Question " + i);
 			scienceQuestions.addLast(("Science Question " + i));
@@ -36,7 +35,6 @@ public class Game {
 		return true;
 	}
 
-	//complicated logic with nested ifs
 	public void roll(int roll) {
         this.currentPlayer = players.get(currentPlayerPosition);
 		System.out.println(  this.currentPlayer.Name + " is the current player");
@@ -62,8 +60,7 @@ public class Game {
 	}
 
 	private void askQuestion() {
-
-		//removeFirst is the same for each linked list. Make a class out of it
+		//TODO: removeFirst is the same for each linked list. Make a class out of it
 		if (Objects.equals(currentCategory(), "Pop"))
 			System.out.println(popQuestions.removeFirst());
 		if (Objects.equals(currentCategory(), "Science"))
@@ -75,7 +72,7 @@ public class Game {
 	}
 
 	private String currentCategory() {
-		//repeated return statements, use case/switch or something better in Java 8
+		//TODO: Gdo something better in Java 8
 		if (this.currentPlayer.currentPosition == 0 || this.currentPlayer.currentPosition == 4 || this.currentPlayer.currentPosition == 8) {
 			return "Pop";
 		} else if (this.currentPlayer.currentPosition == 1 || this.currentPlayer.currentPosition == 5 || this.currentPlayer.currentPosition == 9) {
@@ -93,7 +90,6 @@ public class Game {
 			this.currentPlayer.purseValue++;
 			System.out.println(this.currentPlayer.Name + " now has " + this.currentPlayer.purseValue + " Gold Coins.");
 		}
-
 		currentPlayerPosition = ++currentPlayerPosition % players.size();
 
 		return !this.currentPlayer.isAWinner();
